1e1b7053f6aba79de8203ae0823ff3af04c28264
Marcello Cavalcanti <marcellonovaes98@hotmail.com>
2023-10-16 21:57:07 -0300

    termino dos exercícios do dia 16 de outubro de 2023

--- aula-eventos-de-teclado/script.js
+++ aula-eventos-de-teclado/script.js
@@ -14,5 +14,16 @@ document.addEventListener("keyup", (e)=>{
     navPerfil.style.display = "block";
   } else if (e.code == 'Escape') {
     navPerfil.style.display = "none";
+  } else if(e.code == 'Digit0') {
+    window.location.href = './index.html';
+  }
+})
+
+const linkPerfilDados = document.getElementById('link-perfil-dados')
+/*A fim de não entrar direto na outra página coloquei mais essa etapa*/
+document.addEventListener('keyup', (e) => {
+  if(navPerfil.style.display == "block")
+    if(e.code == 'Digit4') {
+      linkPerfilDados.click();
   }
 })
