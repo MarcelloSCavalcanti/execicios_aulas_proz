00c7435a1e1e5c8bb5aad1c79406d78de16e44ac
Marcello Cavalcanti <marcellonovaes98@hotmail.com>
2023-10-31 12:45:24 -0300

    add new execises

--- aula-eventos-de-teclado/script2.js
+++
@@ -1,7 +0,0 @@
-const voltarPagPrincipal = document.getElementById('voltar-Pag-Principal')
-
-document.addEventListener('keyup', (e) => {
-    if(e.code == 'Digit0') {
-    window.location.href = './index.html';
-    }
-})
